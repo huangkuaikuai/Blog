2ca9619087dde82a7c38881ed24adbbd10a0f155
shuyang <huangtianyi.hty@alibaba-inc.com>
2023-03-05 14:02:56 +0800

    init

--- docs/config.js
+++ docs/config.js
@@ -2,13 +2,4 @@ export default {
   title: 'ShuYang',
   base: '/Blog/',
   description: 'Just playing around.',
-  head: [
-    [
-      "link",
-      {
-        rel: "icon",
-        href: "https://pic3.zhimg.com/80/v2-1b5e55d90a95c42715d4d6fc83606ca6_1440w.webp"
-      }
-    ]
-  ]
 }
